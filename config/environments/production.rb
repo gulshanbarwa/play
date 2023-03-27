55b6bab11db3f7a45e7bba7878f6d5724de7f373
gulshan barwa <gulshanbarwa194@gmail.com>
2023-03-27 18:05:43 +0530

    this is change in developmnt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
     :address               => "smtp.gmail.com",
     :port                  => 587,
     :user_name             => "gulshanconnection@gmail.com",
-    :password              => "lfdaaknjeygpnjsy",
+    :password              => "ejbdhdppcsislyzm",
     :authentication        => "plain",
     :enable_starttls_auto  => true,
   }
